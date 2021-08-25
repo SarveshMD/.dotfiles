3f197f296f82e925b9781fe41617577dc24d4f57
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2021-08-25 10:40:09 +0530

    Created install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir -v $HOME/.config/Code/User
+cp -PRv $HOME/.dotfiles/links/. $HOME
+cp -Pv $HOME/snippets $HOME/settings.json $HOME/keybindings.json $HOME/.config/Code/User/
+rm $HOME/snippets $HOME/settings.json $HOME/keybindings.json
+echo "Task completed successfully..."
+
